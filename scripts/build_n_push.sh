edf34905aae0fb204f64a149485e6999c8b28d2a
Julien Noblet <julien.noblet@gmail.com>
2015-06-02 22:10:22 +0200

    fix Build

--- scripts/build_n_push.sh
+++ scripts/build_n_push.sh
@@ -5,10 +5,12 @@ npm install -g bower gulp
 bower install
 gulp prod
 gulp prod
-cd prod
-git config user.name "Travis-CI"
-git config user.email "travis"
-git add .
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer.git" master:gh-pages 
+cd prod ; \
+git config user.name "Travis-CI" ; \
+git config user.email "travis" ; \
+git add . ; \
+git commit -m "Deploy to GitHub Pages" ; \
+git push --force --quiet \
+  "https://${user}:${password}@github.com/julien-noblet/cad-killer.git"\
+  master:gh-pages 
 #> /dev/null 2>&1
