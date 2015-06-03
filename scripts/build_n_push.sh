fb62b589f324fd61ebfcf17e9f4001f678190178
Travis-CI <travis>
2015-06-03 05:58:47 +0200

    Fix push gh-pages script

--- scripts/build_n_push.sh
+++ scripts/build_n_push.sh
@@ -4,13 +4,13 @@ npm install
 npm install -g bower gulp
 bower install
 gulp prod
-gulp prod
-cd prod ; \
-git config user.name "Travis-CI" ; \
-git config user.email "travis" ; \
-git add . ; \
-git commit -m "Deploy to GitHub Pages" ; \
-git push --force --quiet \
-  "https://${user}:${password}@github.com/julien-noblet/cad-killer.git"\
-  master:gh-pages 
+gulp prod # run twice :)
+( cd prod ;
+  git init ;
+  git config user.name "Travis-CI" ;
+  git config user.email "travis" ;
+  git add . ;
+  git commit -m "Deploy to GitHub Pages" ;
+  git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer.git" master:gh-pages
+)
 #> /dev/null 2>&1
