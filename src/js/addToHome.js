bc360359c9552abf672f278bd37310fc8dda87cf
Julien Noblet <julien.noblet@gmail.com>
2015-06-22 20:50:58 +0200

    AddToHomescreen no limit :)

--- src/js/addToHome.js
+++ src/js/addToHome.js
@@ -3,6 +3,7 @@ addToHomescreen({
   startDelay: 5,
   displayPace:0,
   detectHomescreen: 'smartURL',
+  maxDisplayCount: 0,
   skipFirstVisit: false,
   appID: 'io.github.julien-noblet.cad-killer'
 });
