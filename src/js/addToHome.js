ee4c01059a81522de26d1fd8d03c9c5f5e6b2534
Julien Noblet <julien.noblet@gmail.com>
2015-06-22 22:51:20 +0200

    Revert "AddToHomescreen no limit :)"
    This reverts commit bc360359c9552abf672f278bd37310fc8dda87cf.

--- src/js/addToHome.js
+++ src/js/addToHome.js
@@ -3,7 +3,6 @@ addToHomescreen({
   startDelay: 5,
   displayPace:0,
   detectHomescreen: 'smartURL',
-  maxDisplayCount: 0,
   skipFirstVisit: false,
   appID: 'io.github.julien-noblet.cad-killer'
 });
