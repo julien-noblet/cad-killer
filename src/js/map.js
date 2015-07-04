f6d3b7fb5cf732bf354f2acf639f7204606e1942
Julien Noblet <julien.noblet@gmail.com>
2015-07-04 19:14:12 +0200

    AddBugNote : Let's Rocks :feelsgood:

--- src/js/map.js
+++ src/js/map.js
@@ -7,7 +7,8 @@
   baseMaps,
   layerOSMfr,
   photonControlOptions,
-  searchPoints
+  searchPoints,
+  notesControl
    */
 /**
  * Un grand merci a @etalab, @yohanboniface, @cquest sans qui ce projet n'existerai pas.
@@ -18,7 +19,6 @@ var map = L.map("map", {
   attributionControl: false
 });
 
-var notesItems = new L.FeatureGroup();
 
 var layers = L.control.layers(baseMaps, overlayMaps);
 var myPhoton = new L.Control.Photon(photonControlOptions);
@@ -63,34 +63,6 @@ new L.Control.ReverseLabel().addTo(map);
 
 map.addControl(new GeoLoc());
 
-
-var notesControl = new L.Control.Draw({
-  edit: false,
-  draw: {
-    polyline: false,
-    polygon: false,
-    rectangle: false,
-    circle: false
-  }
-});
 map.addControl(notesControl);
 
-layers.addOverlay(notesItems, "notes");
-
-map.on("draw:created", function(e) {
-  "use strict";
-  var type = e.layerType,
-    layer = e.layer;
-
-  if (type === "marker") {
-    layer.bindPopup("Nouvelle note:");
-  }
-
-  console.log("test", myPhoton );
-  document.getElementById("noteholder").className = "noteholder";
-  /*eslint-disable no-underscore-dangle */
-  document.getElementById("noteref").innerHTML = layer._latlng.lat;
-  /*eslint-enable no-underscore-dangle */
-  notesItems.addLayer(layer);
-  map.addLayer(notesItems);
-});
+//layers.addOverlay(notesItems, "Ma note");
