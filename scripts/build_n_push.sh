32f7c56654e3ac34a6d6056a2d7330f62b770aea
Julien Noblet <julien.noblet+github@gmail.com>
2015-07-06 09:23:40 +0200

    Update build_n_push.sh
    Try to fix #15

--- scripts/build_n_push.sh
+++ scripts/build_n_push.sh
@@ -8,7 +8,7 @@
   echo "Branche : ${TRAVIS_BRANCH}" ;
   echo "PR : ${TRAVIS_PULL_REQUEST}" ;
   git commit -m "Deploy ${TRAVIS_BRANCH} to GitHub Pages" ;
-  if ${TRAVIS_BRANCH} == "master" && !${TRAVIS_PULL_REQUEST}
+  if ${TRAVIS_BRANCH} == "master"
     then git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer.git" master:gh-pages
     else git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer-beta.git" master:gh-pages
   fi
