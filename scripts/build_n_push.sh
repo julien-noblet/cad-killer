88140e004c59975e359877f58a1b910f9c68ddc3
Julien Noblet <julien.noblet+github@gmail.com>
2015-07-06 09:32:17 +0200

    More verbose

--- scripts/build_n_push.sh
+++ scripts/build_n_push.sh
@@ -8,9 +8,13 @@
   echo "Branche : ${TRAVIS_BRANCH}" ;
   echo "PR : ${TRAVIS_PULL_REQUEST}" ;
   git commit -m "Deploy ${TRAVIS_BRANCH} to GitHub Pages" ;
-  if [ ${TRAVIS_BRANCH} == "master" ]
-    then git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer.git" master:gh-pages
-    else git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer-beta.git" master:gh-pages
+  if [ ${TRAVIS_BRANCH} == "master" ] 
+    then 
+      echo "Push to julien-noblet/cad-killer";
+      git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer.git" master:gh-pages;
+    else 
+      echo "Push to julien-noblet/cad-killer-beta";
+      git push --force --quiet "https://${user}:${password}@github.com/julien-noblet/cad-killer-beta.git" master:gh-pages
   fi
 
 #> /dev/null 2>&1
