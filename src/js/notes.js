9306a2e939b92d24f77bd57be00f21e9e8fb52e9
Julien Noblet <julien.noblet@gmail.com>
2015-08-10 12:14:53 +0200

    Better Notes

--- src/js/notes.js
+++ src/js/notes.js
@@ -16,43 +16,52 @@ var notesControl = new L.Control.Draw({
 var notesItems = new L.FeatureGroup();
 
 /*eslint-disable no-unused-vars*/
-var addNote = function() {
+var addNote = function(lat, lng, city, message) {
   "use strict";
-  var lat = document.getElementById("lat").value;
-  var lng = document.getElementById("lng").value;
-  var note = document.getElementById("textnote").value;
-
-  var path = "/api/0.6/notes";
-  var API = "http://api.openstreetmap.org" + path;
-  var content = "?lat=" + lat + "&lon=" + lng + "&text=" + encodeURIComponent(note);
-  var postUrl = API + content;
-  var options = {
-    "method": "POST",
-    "headers": {
-      "Authorization": "Basic " + "Q0FELUtJTExFUjpkdHl2dWRlbnQ="
-    }
-  };
-
-  $.ajax({
-    url: API + content,
-    type: "post",
-    headers: options.headers,
-    success: function(data) {
-      ga("send", "event", "element", "note", "post:" + data, 0);
-
-      document.getElementById("newnote").className += " hidden";
-      document.getElementById("noteok").className = "noteok";
-      document.getElementById("noteholder").className = "noteholder";
-      /*eslint-disable no-console*/
-      console.info(data);
-      /*eslint-enable no-console*/
-    }
-  });
+  /*eslint-disable no-console*/
+  console.log(lat, lng);
+  console.log(city);
+  console.log(message);
+  /*eslint-enable no-console*/
 };
+
+// var addNote = function() {
+//   "use strict";
+//   var lat = document.getElementById("lat").value;
+//   var lng = document.getElementById("lng").value;
+//   var note = document.getElementById("textnote").value;
+//
+//   var path = "/api/0.6/notes";
+//   var API = "http://devapi.openstreetmap.org" + path;
+//   var content = "?lat=" + lat + "&lon=" + lng + "&text=" + encodeURIComponent(note);
+//   var postUrl = API + content;
+//   var options = {
+//     "method": "POST",
+//     "headers": {
+//       "Authorization": "Basic " + "Q0FELUtJTExFUjpkdHl2dWRlbnQ="
+//     }
+//   };
+//
+//   $.ajax({
+//     url: API + content,
+//     type: "post",
+//     headers: options.headers,
+//     success: function(data) {
+//       ga("send", "event", "element", "note", "post:" + data, 0);
+//
+//       document.getElementById("newnote").className += " hidden";
+//       document.getElementById("noteok").className = "noteok";
+//       document.getElementById("noteholder").className = "noteholder";
+//       /*eslint-disable no-console*/
+//       console.info(data);
+//       /*eslint-enable no-console*/
+//     }
+//   });
+// };
 /*eslint-enable no-unused-vars*/
 
 /*eslint-disable no-unused-vars*/
-var resetNote = function(){
+var resetNote = function() {
   "use strict";
   document.getElementById("noteholder").className = "noteholder hidden";
   document.getElementById("noteok").className = "noteok hidden";
@@ -68,27 +77,74 @@ map.on("draw:created", function(e) {
 
   //console.log("test", myPhoton );
   /*eslint-disable no-underscore-dangle */
-  var req = $.ajax(REVERSE_URL + "lon=" + layer._latlng.lng + "&lat=" + layer._latlng.lat, {
+  var url = [REVERSE_URL, "lon=", layer._latlng.lng, "&lat=", layer._latlng.lat].join("");
+  var req = $.ajax(url, {
     dataType: "json"
   });
   /*eslint-enable no-underscore-dangle */
+  L.Util.ajax(url).then(function(data) {
+    var city = "";
 
-  req.done(function(msg) {
-    var city = msg.features[0].properties.city;
-    /*eslint-disable no-underscore-dangle */
-    document.getElementById("lat").value = layer._latlng.lat;
-    document.getElementById("lng").value = layer._latlng.lng;
-    /*eslint-enable no-underscore-dangle */
-    document.getElementById("noteholder").className = "noteholder";
-    document.getElementById("noteref").innerHTML = city;
+    if (data.features[0]) {
+      city = ["<span class=\"city\">\n près de ", data.features[0].properties.city, "<span>"].join("");
+    }
     notesItems.addLayer(layer);
     map.addLayer(notesItems);
-  });
-  req.fail(function(msg) {
+
+    map.fire("modal", {
+      content: ["<textarea id=\"textnote\" class=\"textnote\" name=\"textnote\" autofocus=\"yes\"></textarea>"].join(""), // HTML string
+      title: ["Nouvelle demande de correction de la carte", city, " :"].join(""),
+      template: ["<div class=\"modal-header\"><h2>{title}</h2></div>",
+        "<hr>",
+        "<div class=\"modal-body\">{content}</div>",
+        "<div class=\"modal-footer\">",
+        /*eslint-disable no-underscore-dangle */
+        "<input type=\"hidden\" id=\"lat\" name=\"lat\" value=\"", layer._latlng.lat, "\">",
+        "<input type=\"hidden\" id=\"lng\" name=\"lng\" value=\"", layer._latlng.lng, "\">",
+        /*eslint-disable no-underscore-dangle */
+        "<input type=\"submit\" class=\"topcoat-button--large\" value=\"{okText}\" ",
+        "onclick=\" {addNote} \">",
+        /*eslint-enable no-underscore-dangle */
+        "<input type=\"submit\" class=\"topcoat-button--large {CLOSE_CLS}\" value=\"{cancelText}\" onclick=\"map.closeModal();\">",
+        "</div>"
+      ].join(""),
+      addNote: ["",
+        "addNote( \"", layer._latlng.lat,
+        "\", \"", layer._latlng.lng,
+        "\", \"", (data.features[0]) ? data.features[0].properties.city : "",
+        "\", \"",
+        "textnote",
+        "\" );"
+      ].join(""),
+      okText: "Soumettre la note",
+      cancelText: "Annuler",
+      closeTitle: "Fermer", // alt title of the close button
+      zIndex: 10000, // needs to stay on top of the things
+      transitionDuration: 300, // expected transition duration
+      // callbacks for convenience,
+      // you can set up you handlers here for the contents
+      /*eslint-disable no-console */
+      //onShow: function(evt){ var modal = evt.modal; console.log(modal); },
+      onHide: function() {
+        map.removeLayer(notesItems);
+      },
+      /*eslint-enable no-console */
+
+      // change at your own risk
+      OVERLAY_CLS: "overlay", // overlay(backdrop) CSS class
+      MODAL_CLS: "modal", // all modal blocks wrapper CSS class
+      MODAL_CONTENT_CLS: "modal-content", // modal window CSS class
+      INNER_CONTENT_CLS: "modal-inner", // inner content wrapper
+      SHOW_CLS: "show", // `modal open` CSS class, here go your transitions
+      CLOSE_CLS: "close" // `x` button CSS class
+    });
+
     /*eslint-disable no-console*/
-    console.log(msg);
-    /*eslint-enable no-console*/
+    console.log(data);
+    console.log(data.features[0].properties.city);
+
   });
+
 });
 
 //layers.addOverlay(notesItems, "Ma note");
