ba82bdb0023ef462b6f67dc0cb85b31229e8e208
Julien Noblet <julien.noblet@gmail.com>
2016-04-24 14:48:18 +0200

    backstop update

---
+++ backstop_data/scripts/test.js
@@ -0,0 +1,11 @@
+/*module.exports = function(casper, scenario, vp) {
+  casper.echo( 'Clicking button ' );
+  casper.click( '.toggle' );
+  if (vp.name === 'phone') {
+    // do stuff for just phone viewport here
+    //   }
+    //     // do other cool stuff here, see Casperjs.org for a full API and many ideas.
+    //       // scenario is the current scenario object being run from your backstop.json file
+    //
+    }
+*/
