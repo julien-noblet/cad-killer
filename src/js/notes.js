b60f78dbb9c32b2762b4cd1d61beb43e5343977c
Julien Noblet <julien.noblet@gmail.com>
2017-11-17 11:22:59 +0100

    Fix Circle marker
    Circlemarker is not needed.

--- src/js/notes.js
+++ src/js/notes.js
@@ -15,10 +15,11 @@ require("leaflet-ajax");
 const notesControl = new L.Control.Draw({
   edit: false,
   draw: {
-    polyline: false,
+    circle: false,
+    circlemarker: false,
     polygon: false,
-    rectangle: false,
-    circle: false
+    polyline: false,
+    rectangle: false
   }
 });
 /* eslint-enable no-unused-vars */
@@ -108,8 +109,9 @@ Window.map.on("draw:created", e => {
     let city = "";
     if (data.features[0]) {
       city = data.features[0].properties.city;
-      HtmlCity = `<span class="city">\n près de ${data.features[0].properties
-        .city}<span>`;
+      HtmlCity = `<span class="city">\n près de ${
+        data.features[0].properties.city
+      }<span>`;
     }
     notesItems.addLayer(layer);
     Window.map.addLayer(notesItems);
@@ -121,14 +123,18 @@ Window.map.on("draw:created", e => {
       })
       .setContent(
         [
-          `<div class="modal-header"><h2>Nouvelle demande de correction de la carte ${HtmlCity} :</h2></div>`,
+          `<div class="modal-header"><h2>Nouvelle demande de correction de la carte ${
+            HtmlCity
+          } :</h2></div>`,
           "<hr>",
           '<div class="modal-body"><textarea id="textnote" class="textnote" name="textnote" autofocus="yes"></textarea></div>',
           '<div class="modal-footer">',
-          `<input type="hidden" id="lat" name="lat" value="${layer._latlng
-            .lat}">`,
-          `<input type="hidden" id="lng" name="lng" value="${layer._latlng
-            .lng}">`,
+          `<input type="hidden" id="lat" name="lat" value="${
+            layer._latlng.lat
+          }">`,
+          `<input type="hidden" id="lng" name="lng" value="${
+            layer._latlng.lng
+          }">`,
           '<input type="submit" class="topcoat-button--large" value="Soumettre la note" ',
           'onclick=" Window.addNote() ">',
           '<input type="submit" class="topcoat-button--large close" value="Annuler" onclick="Window.dialog.close();">',
