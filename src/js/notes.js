011bd42a477d3f95fcd975eab17e3e0f0492ed5e
Julien Noblet <julien.noblet@gmail.com>
2017-11-20 09:44:50 +0100

    better flow coverage for notes.js

--- src/js/notes.js
+++ src/js/notes.js
@@ -27,35 +27,39 @@ const notesControl = new L.Control.Draw({
 const notesItems = new L.FeatureGroup();
 
 /* eslint-disable no-unused-vars */
-Window.addNote = function addNote() {
+Window.addNote = function addNote(): void {
   let eLat = document.getElementById("lat");
   let eLng = document.getElementById("lng");
   let eNote = document.getElementById("textnote");
-
+  console.log(eLat, eLng, eNote);
   if (
+    eLat instanceof HTMLInputElement &&
+    eLng instanceof HTMLInputElement &&
+    eNote instanceof HTMLTextAreaElement &&
     parseFloat(eLat.value) !== null &&
     parseFloat(eLng.value) !== null &&
     eNote.value !== null &&
-    parseFloat(eLat.value) !== 0 &&
+    parseFloat(eLat.value) == 0 &&
     parseFloat(eLng.value) !== 0 &&
     eNote.value !== ""
   ) {
     const lat: number = parseFloat(eLat.value);
     const lng: number = parseFloat(eLng.value);
     const note: string = eNote.value;
-    const content = `?lat=${lat}&lon=${lng}&text=${encodeURIComponent(note)}`;
-    const options = {
+    const content: string = `?lat=${lat}&lon=${lng}&text=${encodeURIComponent(
+      note
+    )}`;
+    const options: { headers: { Authorization: string }, method: string } = {
       method: "POST",
       headers: {
         Authorization: "Basic $OSM_CREDITENTIALS"
       }
     };
-    $.ajax({
-      /* TODO: Changer pour L.Util.ajax() pour supprimer la dépendece avec Jquery */
+    const settings: JQueryAjaxSettings = {
       url: NOTE_API + content,
       type: "post",
       headers: options.headers,
-      error: data => {
+      error: function(data, textStatus, err) {
         Window.dialog.setContent(
           '<h1>Votre note n\'à pas été envoyée <i class="zmdi zmdi-mood-bad"></i></h1><br/>Veuillez ré-essayer ultérieurement.'
         );
@@ -66,7 +70,7 @@ Window.addNote = function addNote() {
           Window.dialog.close();
         }, 5000); // on ferme après 5 sec
       },
-      success: data => {
+      success: function(data) {
         /* eslint-disable max-len */
         Window.dialog.setContent(
           '<h1>Votre note à été envoyée <i class="zmdi zmdi-mood"></i></h1><br/>Merci pour votre contribution.'
@@ -80,7 +84,10 @@ Window.addNote = function addNote() {
 
         // TODO: Window.dialog.destroy(); // cela reduira l'empreinte mémoire.
       }
-    });
+    };
+    $.ajax(
+      settings
+    ); /* TODO: Changer pour L.Util.ajax() pour supprimer la dépendece avec Jquery */
   }
 };
 /* eslint-enable no-unused-vars */
@@ -100,13 +107,13 @@ Window.map.on("draw:created", e => {
   const layer = e.layer;
   const url = `${REVERSE_URL}lon=${layer._latlng.lng}&lat=${layer._latlng.lat}`;
   /* eslint-disable no-unused-vars */
-  const req = $.ajax(url, {
+  const req: JQueryXHR = $.ajax(url, {
     dataType: "json"
   });
   /* eslint-enable no-unused-vars */
   L.Util.ajax(url).then(data => {
-    let HtmlCity = "";
-    let city = "";
+    let HtmlCity: string = "";
+    let city: string = "";
     if (data.features[0]) {
       city = data.features[0].properties.city;
       HtmlCity = `<span class="city">\n près de ${
