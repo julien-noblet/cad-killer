e03c51cd23dec23c43411009576cc1c9432d8964
Julien Noblet <julien.noblet@gmail.com>
2018-04-10 09:33:20 +0200

    Fix stats

--- src/js/config.js
+++ src/js/config.js
@@ -26,7 +26,7 @@ export const IGN_KEY = "zh4iqh58bg7a17hqzo16nqfw";
 export const IGN_LAYER = "GEOGRAPHICALGRIDSYSTEMS.MAPS";
 export const IGN_LAYER_LITE = "GEOGRAPHICALGRIDSYSTEMS.PLANIGN";
 export const MY_POUCHDB =
-  "https://sidercomourellydiforteep:16f21a8ad23f32de280b36fc338a38a24c5d49b2@cad-killer.cloudant.com/stats";
+  "https://sidercomourellydiforteep:16f21a8ad23f32de280b36fc338a38a24c5d49b2@couchdb-112956.smileupps.com/stats";
 export const LOCAL_POUCHDB = "cad-killer";
 export const OSM_CREDITENTIALS = "Q0FELUtJTExFUjpkdHl2dWRlbnQ=";
 export const NOTE_API = "http://api.openstreetmap.org/api/0.6/notes";
