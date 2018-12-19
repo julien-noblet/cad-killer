488b0ec470e547856ba23f0ec7e090e7e96fb109
Travis CI User <travis@example.org>
2018-12-19 13:19:41 +0000

    chore(package): update lockfiles yarn.lock & package-lock.json + update flow-typed files

--- flow-typed/npm/jquery_v3.x.x.js
+++ flow-typed/npm/jquery_v3.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: d71d314ca25fc6c20610a3ba80af9df0
-// flow-typed version: 9698a46399/jquery_v3.x.x/flow_>=v0.28.x
+// flow-typed signature: 44a52f7267ac63ff00c5676e5984862d
+// flow-typed version: 7590a5d0c6/jquery_v3.x.x/flow_>=v0.28.x
 
 /* eslint-disable max-len, no-unused-vars, flowtype/no-weak-types */
 
@@ -822,7 +822,7 @@ declare class JQueryStatic {
    */
   get(
     url: string,
-    data?: Object | string,
+    data?: {} | string,
     success?: (data: any, textStatus: string, jqXHR: JQueryXHR) => any,
     dataType?: string
   ): JQueryXHR;
@@ -845,7 +845,7 @@ declare class JQueryStatic {
    */
   getJSON(
     url: string,
-    data?: Object | string,
+    data?: {} | string,
     success?: (data: any, textStatus: string, jqXHR: JQueryXHR) => any
   ): JQueryXHR;
   /**
@@ -886,7 +886,7 @@ declare class JQueryStatic {
    */
   post(
     url: string,
-    data?: Object | string,
+    data?: {} | string,
     success?: (data: any, textStatus: string, jqXHR: JQueryXHR) => any,
     dataType?: string
   ): JQueryXHR;
@@ -967,7 +967,7 @@ declare class JQueryStatic {
    * @param html A string defining a single, standalone, HTML element (e.g. <div/> or <div></div>).
    * @param attributes An object of attributes, events, and methods to call on the newly-created element.
    */
-  (html: string, attributes: Object): JQuery;
+  (html: string, attributes: {}): JQuery;
 
   /**
    * Relinquish jQuery's control of the $ variable.
@@ -1425,7 +1425,7 @@ declare class JQuery {
    */
   load(
     url: string,
-    data?: string | Object,
+    data?: string | {},
     complete?: (
       responseText: string,
       textStatus: string,
@@ -1482,7 +1482,7 @@ declare class JQuery {
    *
    * @param attributes An object of attribute-value pairs to set.
    */
-  attr(attributes: Object): JQuery;
+  attr(attributes: {}): JQuery;
   /**
    * Get the value of an attribute for the first element in the set of matched elements.
    *
@@ -1532,7 +1532,7 @@ declare class JQuery {
    *
    * @param properties An object of property-value pairs to set.
    */
-  prop(properties: Object): JQuery;
+  prop(properties: {}): JQuery;
   /**
    * Set one or more properties for the set of matched elements.
    *
@@ -1634,7 +1634,7 @@ declare class JQuery {
    *
    * @param properties An object of property-value pairs to set.
    */
-  css(properties: Object): JQuery;
+  css(properties: {}): JQuery;
   /**
    * Get the value of style properties for the first element in the set of matched elements.
    *
@@ -1847,7 +1847,7 @@ declare class JQuery {
    * @param complete A function to call once the animation is complete.
    */
   animate(
-    properties: Object,
+    properties: {},
     duration?: string | number,
     complete?: Function
   ): JQuery;
@@ -1860,7 +1860,7 @@ declare class JQuery {
    * @param complete A function to call once the animation is complete.
    */
   animate(
-    properties: Object,
+    properties: {},
     duration?: string | number,
     easing?: string,
     complete?: Function
@@ -1871,7 +1871,7 @@ declare class JQuery {
    * @param properties An object of CSS properties and values that the animation will move toward.
    * @param options A map of additional options to pass to the method.
    */
-  animate(properties: Object, options: JQueryAnimationOptions): JQuery;
+  animate(properties: {}, options: JQueryAnimationOptions): JQuery;
 
   /**
    * Set a timer to delay execution of subsequent items in the queue.
@@ -2920,14 +2920,14 @@ declare class JQuery {
    * @param eventType A string containing a JavaScript event type, such as click or submit.
    * @param extraParameters Additional parameters to pass along to the event handler.
    */
-  trigger(eventType: string, extraParameters?: any[] | Object): JQuery;
+  trigger(eventType: string, extraParameters?: any[] | {}): JQuery;
   /**
    * Execute all handlers and behaviors attached to the matched elements for the given event type.
    *
    * @param event A jQuery.Event object.
    * @param extraParameters Additional parameters to pass along to the event handler.
    */
-  trigger(event: JQueryEventObject, extraParameters?: any[] | Object): JQuery;
+  trigger(event: JQueryEventObject, extraParameters?: any[] | {}): JQuery;
 
   /**
    * Execute all handlers attached to an element for an event.
