3e8ae07299b4530afa173e664871f167ccd333d2
Julien Noblet <julien.noblet@gls-france.com>
2022-02-07 20:44:42 +0100

    Chore: Upgrade deps

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: "./src/index.html",
   filename: "index.html",
@@ -101,17 +101,16 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.optimize\.css$/g,
-      cssProcessor: require("cssnano"),
-      cssProcessorOptions: { discardComments: { removeAll: true } },
-      canPrint: true,
-    }),
     new CopyWebpackPlugin({ patterns: [{ from: "static" }] }),
+    new MiniCssExtractPlugin(),
   ],
   optimization: {
     minimize: true,
-
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
     //runtimeChunk: true,
     splitChunks: {
       chunks: "async",
