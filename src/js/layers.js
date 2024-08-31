29254d5a2791f157cc9b0c2bd75fd02ad04f5d40
Julien Noblet <julien.noblet@gmail.com>
2024-08-31 23:20:28 +0200

    suppression des fichier inutiles + correction layers.js

--- src/js/layers.js
+++ src/js/layers.js
@@ -6,7 +6,7 @@ import L from "leaflet";
 import { IGN_KEY, IGN_LAYER } from "./config";
 
 // Layers
-export const layerOSMfr: any = L.tileLayer(
+export const layerOSMfr = L.tileLayer(
   "//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
   {
     maxZoom: 20,
@@ -15,7 +15,7 @@ export const layerOSMfr: any = L.tileLayer(
   }
 );
 
-export const layerOSM: any = L.tileLayer(
+export const layerOSM = L.tileLayer(
   "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   {
     maxZoom: 19,
@@ -24,7 +24,7 @@ export const layerOSM: any = L.tileLayer(
   }
 );
 
-export const layerCadastre: any = L.tileLayer(
+export const layerCadastre = L.tileLayer(
   "http://tms.cadastre.openstreetmap.fr/*/tout/{z}/{x}/{y}.png",
   {
     maxZoom: 22,
@@ -33,7 +33,7 @@ export const layerCadastre: any = L.tileLayer(
   }
 );
 
-export const overlayCadastre: any = L.tileLayer(
+export const overlayCadastre = L.tileLayer(
   "http://tms.cadastre.openstreetmap.fr/*/transp/{z}/{x}/{y}.png",
   {
     maxZoom: 22,
@@ -42,7 +42,7 @@ export const overlayCadastre: any = L.tileLayer(
   }
 );
 
-export const layerEsriWorldImagery: any = L.tileLayer(
+export const layerEsriWorldImagery = L.tileLayer(
   "//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   {
     attribution:
@@ -51,7 +51,7 @@ export const layerEsriWorldImagery: any = L.tileLayer(
 );
 
 
-export const layerEsriWorldStreetMap: any = L.tileLayer(
+export const layerEsriWorldStreetMap = L.tileLayer(
   "//server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
   {
     attribution:
@@ -60,7 +60,7 @@ export const layerEsriWorldStreetMap: any = L.tileLayer(
   }
 );
 
-export const layerIGN: any = L.tileLayer(
+export const layerIGN = L.tileLayer(
   `//data.geopf.fr/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=${IGN_LAYER}&STYLE=normal&FORMAT=image/jpeg&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}`,
  // `//wxs.ign.fr/${IGN_KEY}/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=${IGN_LAYER}&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg`,
   {
@@ -71,7 +71,7 @@ export const layerIGN: any = L.tileLayer(
   }
 );
 
-export const overlayBAN: any = L.tileLayer(
+export const overlayBAN = L.tileLayer(
   "//{s}.layers.openstreetmap.fr/bano/{z}/{x}/{y}.png",
   {
     maxZoom: 20,
