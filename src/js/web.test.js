fdd37665c2ba7f871b1f6ef7c0be7f25b80a48e6
Julien Noblet <julien.noblet@gmail.com>
2024-09-01 02:36:03 +0200

    WIP

--- src/js/web.test.js
+++ src/js/web.test.js
@@ -1,15 +1,16 @@
 /** @format */
 
 import "jest";
+//import puppeteer, { Browser } from "puppeteer";
+//import { compile } from "handlebars";
+//import { run } from "jest/build";
 var path = require("path");
-import puppeteer, { Browser } from "puppeteer";
-import webpack from "webpack";
-import WebpackDevServer from "webpack-dev-server";
-import { compile } from "handlebars";
-const { toMatchImageSnapshot } = require("jest-image-snapshot");
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('../../webpack.config.js');
 
 // Extend Jest expect
-expect.extend({ toMatchImageSnapshot });
+const timeout = 10*60*1000; // 10 minutes
 
 const viewports = [
   { width: 320, height: 200 },
@@ -20,77 +21,78 @@ const viewports = [
   { width: 1280, height: 960 },
 ];
 
-describe("Web Render test", () => {
-  let server;
-  let browser;
-  let page;
 
+const compiler = Webpack(webpackConfig);
+const devServerOptions = { ...webpackConfig.devServer, open: true };
+const server = new WebpackDevServer(devServerOptions, compiler);
+
+const runServer = async () => {
+  console.log('Starting server...');
+  await server.start();
+};
+
+describe("Web Render test", () => {
+  let page;
+  
   /**
    * Initialize browser and server
    */
   beforeAll(async () => {
-    jest.setTimeout(10 * 60 * 1000); // 5 min allow test to run for longer time so they don't timeout
-    //await browser.ignoreHTTPSErrors = true //force
+    
+    runServer();
+
+    page = await globalThis.__BROWSER_GLOBAL__.newPage();
+    await page.goto('http://localhost:9000/');
+  }, timeout);
+
+  afterAll(async () => {
+    // close the server
+    await server.stop();
+    // close the browser
+    await page.close();
   });
 
-  test("Correctly renders header", async () => {
+  it("It correctly render the header", async () => {
     for (const viewport of viewports) {
       await page.setViewport(viewport);
-      await page.goto(`http://localhost:9000/`, { timeout: 300000 });
+      await page.goto(`http://localhost:9000/`);
       const elem = await page.$("#head");
       const screenshot = await elem.screenshot({
         // clip: { x: 0, y: 0, width: viewport.width, height: 60 }
       });
       expect(screenshot).toMatchImageSnapshot({
+        runInProcess: true ,
         customSnapshotIdentifier:
           "header_" + viewport.width + "x" + viewport.height,
         customDiffConfig: { threshold: 10 },
       });
     }
-  });
-  test.skip("Correctly renders header for mobiles", async () => {
-    for (const device of puppeteer.devices) {
-      await page.emulate(device);
-      await page.goto(`http://localhost:9000/`);
-      const elem = await page.$("#head");
-      const screenshot = await elem.screenshot({
-        //        clip: { x: 0, y: 0, width: device.viewport.width, height: 60 }
-      });
-      expect(screenshot).toMatchImageSnapshot({
-        customSnapshotIdentifier: "header_" + device.name,
-      });
-    }
-  });
-  test("Correctly renders main page", async () => {
+  }
+  );
+  it("Correctly renders main page", async () => {
     for (const viewport of viewports) {
       await page.setViewport(viewport);
       await page.goto(`http://localhost:9000/`, { timeout: 300000 });
+      // sleep for 5 seconds
+      await page.evaluate(() => document.body.textContent);
+
       const screenshot = await page.screenshot({
         // no clip: { x: 0, y: 0, width: viewport.width, height: 60 }
       });
       expect(screenshot).toMatchImageSnapshot({
+        runInProcess: true ,
+        comparisonMethod: 'ssim',
+        failureThreshold: 0.01,
+        failureThresholdType: 'percent',
+        maxChildProcessBufferSizeInBytes: 100*1024*1024,
         customSnapshotIdentifier:
           "main_" + viewport.width + "x" + viewport.height,
         customDiffConfig: { threshold: 10 },
       });
     }
   });
-  test.skip("Correctly renders mobile main page", async () => {
-    for (const device of puppeteer.devices) {
-      const page = await browser.newPage();
-      await page.emulate(device);
-      await page.goto(`http://localhost:9000/`, { timeout: 300000 });
-      const screenshot = await page.screenshot({
-        //no clip: { x: 0, y: 0, width: device.viewport.width, height: 60 }
-      });
-      expect(screenshot).toMatchImageSnapshot({
-        customSnapshotIdentifier: "main_" + device.name,
-        customDiffConfig: { threshold: 10 },
-      });
-    }
-  });
 
-  test("List Layers", async () => {
+  it("List Layers", async () => {
     await page.setViewport({ width: 1280, height: 960 });
     await page.goto(`http://localhost:9000/`, { timeout: 300000 });
     await page.waitForSelector(".leaflet-control-layers-toggle", {
@@ -111,12 +113,13 @@ describe("Web Render test", () => {
 
     const screenshot = await elem.screenshot();
     expect(screenshot).toMatchImageSnapshot({
+      runInProcess: true ,
       customSnapshotIdentifier: "layers_list",
       customDiffConfig: { threshold: 10 },
     });
   });
 
-  test("Attribution", async () => {
+  it("Attribution", async () => {
     await page.setViewport({ width: 1280, height: 960 });
     await page.goto(`http://localhost:9000/`, { timeout: 300000 });
 
@@ -128,8 +131,39 @@ describe("Web Render test", () => {
 
     const screenshot = await elem.screenshot();
     expect(screenshot).toMatchImageSnapshot({
+      runInProcess: true ,
       customSnapshotIdentifier: "attribution_default",
       customDiffConfig: { threshold: 10 },
     });
+  })
+/* 
+  test.skip("Correctly renders header for mobiles", async () => {
+    for (const device of puppeteer.devices) {
+      await page.emulate(device);
+      await page.goto(`http://localhost:9000/`);
+      const elem = await page.$("#head");
+      const screenshot = await elem.screenshot({
+        //        clip: { x: 0, y: 0, width: device.viewport.width, height: 60 }
+      });
+      expect(screenshot).toMatchImageSnapshot({
+        customSnapshotIdentifier: "header_" + device.name,
+      });
+    }
   });
-});
+  test.skip("Correctly renders mobile main page", async () => {
+    for (const device of puppeteer.devices) {
+      const page = await browser.newPage();
+      await page.emulate(device);
+      await page.goto(`http://localhost:9000/`, { timeout: 300000 });
+      const screenshot = await page.screenshot({
+        //no clip: { x: 0, y: 0, width: device.viewport.width, height: 60 }
+      });
+      expect(screenshot).toMatchImageSnapshot({
+        customSnapshotIdentifier: "main_" + device.name,
+        customDiffConfig: { threshold: 10 },
+      });
+    }
+  });
+
+  ;/**/ 
+},timeout);
