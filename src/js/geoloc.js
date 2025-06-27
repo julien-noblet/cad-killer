c5b6c54d353ba3a0fdf763316808fa420376df57
Julien Noblet <julien.noblet@gmail.com>
2025-06-27 07:19:17 +0200

    refactor(geoloc): améliorer la structure et l'exportation des fonctions de géolocalisation

--- src/js/geoloc.js
+++ src/js/geoloc.js
@@ -3,11 +3,13 @@
  * @module geoloc
  */
 
+import L from "leaflet";
+
 /**
  * Centre la carte sur la position fournie et met à jour l'icône.
  * @param {GeolocationPosition} position
  */
-const showPosition = (position) => {
+function showPosition(position) {
   const icon = document.getElementById("geoloc_icon");
   if (window.map) {
     window.map.setView([
@@ -16,12 +18,12 @@ const showPosition = (position) => {
     ], 16);
     if (icon) icon.className = "zmdi zmdi-2x zmdi-gps-dot";
   }
-};
+}
 
 /**
  * Demande la position de l'utilisateur et met à jour l'icône.
  */
-window.getLocation = () => {
+export function getLocation() {
   const icon = document.getElementById("geoloc_icon");
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, (err) => {
@@ -31,23 +33,34 @@ window.getLocation = () => {
     });
     if (icon) icon.className = "zmdi zmdi-2x zmdi-gps";
   }
-};
+}
 
 /**
  * Contrôle Leaflet pour la géolocalisation
  */
-const GeoLoc = L.Control.extend({
-  options: {
-    position: "topright",
-  },
-  onAdd: function () {
+export class GeoLocControl extends L.Control {
+  constructor(options = {}) {
+    super({ position: "topright", ...options });
+  }
+  onAdd() {
     const container = L.DomUtil.create("div", "leaflet-control-geoloc");
     container.innerHTML =
-      '<span onClick="getLocation();" id="geoloc" class="geoloc"><i class="zmdi zmdi-2x zmdi-gps-off" id="geoloc_icon"></i></span>';
+      '<span id="geoloc" class="geoloc"><i class="zmdi zmdi-2x zmdi-gps-off" id="geoloc_icon"></i></span>';
+    container.addEventListener("click", getLocation);
     return container;
-  },
-});
+  }
+}
 
+/**
+ * Ajoute le contrôle de géolocalisation à la carte si elle existe.
+ */
+export function addGeoLocControlToMap(map) {
+  if (map) {
+    map.addControl(new GeoLocControl());
+  }
+}
+
+// Pour compatibilité avec l'ancien code global
 if (window.map) {
-  window.map.addControl(new GeoLoc());
+  addGeoLocControlToMap(window.map);
 }
